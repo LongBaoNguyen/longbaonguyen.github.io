2037d780027eda1bc809b8dd59f35457bbbcac98
LongBaoNguyen <long.nguyen@umb.edu>
2022-01-13 12:54:19 -0500

    d

--- courses/apcsp/processing_arcade/processing_py/game.py
+++ courses/apcsp/processing_arcade/processing_py/game.py
@@ -4,9 +4,6 @@ Main logic of game is here.
 
 from __future__ import division, print_function
 import arcade
-import random
-
-
 
 WIDTH = 800 # width of screen in pixels
 HEIGHT = 600 # height of screen in pixels
@@ -22,44 +19,24 @@ class Window:
             For example:
             self.lives = 3
             self.score = 0
-                        
+            
             Create Sprite object at the origin, default 1.0 scale.
             self.player = arcade.Sprite("tank.png")
             
             Create Sprite object with 2.0 scale position at (200, 300)
             self.coin = arcade.Sprite("coin.png", 2.0, 200, 300)
         """
-        self.player = arcade.Sprite("tank.png")
-        self.player.center_x = WIDTH/2
-        self.player.center_y = HEIGHT/2
-        
-
-        
-        self.coins = []
-        for i in range(10):
-            # create a coin Sprite, add to list
-            coin = arcade.Sprite("coin.png")
-            coin.center_x = random.randrange(WIDTH)
-            coin.center_y = random.randrange(HEIGHT)
-            self.coins.append(coin)
-
         
         
-        
-
     def on_draw(self):
         """ Called automatically 60 times a second to draw objects.
             Write code to draw all objects.
-        """    
-        self.player.draw()
-        for coin in self.coins:
-            coin.draw()
+        """
         
     def on_update(self):
         """ Called to update our objects about 60 times per second.
             Write code to update all objects(for animation).
         """
-        self.player.update()
         
    
     def on_key_press(self, key):
@@ -78,38 +55,22 @@ class Window:
           elif key == 'b':
               # code to respond to 'b' key being pressed.
         """
-        if key == RIGHT:
-            self.player.change_x = 5
-        elif key == LEFT:
-            self.player.change_x = -5
-        elif key == UP:
-            self.player.change_y = -5 
-        elif key == DOWN:
-            self.player.change_y = 5
-            
-            
-            
-
+        
+        
 
     def on_key_release(self, key):
         """ Called automatically whenever a key is released. 
         """
-        if key == RIGHT:
-            self.player.change_x = 0
-        elif key == LEFT:
-            self.player.change_x = 0
-        elif key == UP:
-            self.player.change_y = 0
-        elif key == DOWN:
-            self.player.change_y = 0
-                    
+        
+        
+            
         
     def on_mouse_press(self, x, y, button):
         """ Called whenever the mouse is pressed. 
             Parameters: x, y location of the mouse
                         button: LEFT, RIGHT, CENTER
         """
-        pass
+        
         
     def on_mouse_release(self, x, y, button):
         """ Called whenever the mouse is released. 
@@ -117,7 +78,7 @@ class Window:
                         button: LEFT, RIGHT, CENTER
 
         """
-        pass
+        
                   
       
         
