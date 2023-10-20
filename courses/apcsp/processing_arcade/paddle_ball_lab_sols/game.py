2e6acde4a693415a2e7d268591a6c449c4bd410d
LongBaoNguyen <long.nguyen@umb.edu>
2023-10-20 08:16:49 -0400

    d

--- courses/apcsp/processing_arcade/paddle_ball_lab_sols/game.py
+++ courses/apcsp/processing_arcade/paddle_ball_lab_sols/game.py
@@ -30,16 +30,20 @@ class Window:
         
         # create two Sprite objects(paddle.png and ball.png, scaling = 0.2)
         # put paddle at y = 500
-        self.paddle = Sprite("paddle.png", 0.2, mouseX, 500)
-        self.ball = Sprite("ball.png", 0.2, 200, 400)
-        
+        self.paddle = Sprite("paddle.png", 0.4, 100, 500)
+        self.ball = Sprite("ball.png", 0.4, 100, 300)
+
+
+
         # set change_x and change_y properties of ball object
+        # use dot notation
         self.ball.change_x = 5
         self.ball.change_y = 5
 
 
 
                                                                                                                                         
+                                                                                                                                                                                                                                                                                                                                                                                                                        
     def on_draw(self):
         """ Called automatically 60 times a second to draw/update objects.
             Write code to draw/update all objects.
@@ -47,23 +51,31 @@ class Window:
         # draw paddle and ball
         self.paddle.draw()
         self.ball.draw()
-        
-        # set paddle position to equal to follow mouseX but at fixed y position(500)
+
+        # set paddle position(center_x, center_y) to follow mouseX but at fixed y position(500)
         self.paddle.center_x = mouseX
+        
         # make ball move
         self.ball.move()
-        
+
         # if ball pass right screen or left screen, negate change_x 
-        if self.ball.center_x < 0 or self.ball.center_x > WIDTH:
+        # use get_left() and get_right() functions
+        if self.ball.get_left() < 0 or self.ball.get_right() > WIDTH:
             self.ball.change_x *= -1
-        
-        # if ball pass top screen or bottom screen, negate change_y 
-        if self.ball.center_y < 0 or self.ball.center_y > HEIGHT:
+            
+        # if ball pass top screen or bottom screen, negate change_y
+        # use get_top() and get_bottom() functions
+        if self.ball.get_top() < 0 or self.ball.get_bottom() > HEIGHT:
             self.ball.change_y *= -1
-        
+
         
         # make ball bounce off top of paddle(Hint: Use get_top(), get_right() etc.., see arcade.py)
+        passed_top_paddle = self.ball.get_bottom() > self.paddle.get_top()
+        in_between_paddle = self.paddle.get_left() < self.ball.center_x < self.paddle.get_right()
    
+        if passed_top_paddle and in_between_paddle:
+            self.ball.change_y *= -1
+        
     def on_key_press(self, key):
         """ Called automatically whenever a key is pressed. 
         Example:
